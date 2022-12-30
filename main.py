af9926d0bf4c9d28da3736f747397f17c3018b8a
woodman <shutov.dev@proton.me>
2022-12-30 10:46:29 +0200

    fix recursion error

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import datetime
 from time import sleep
-from icmplib import ping
 import telegram
+from icmplib import ping
 
 TG_TOKEN = "5857992954:AAEXOiADgCI5LkBd5CpnWrasDjhNkH75j3I"
 bot = telegram.Bot(TG_TOKEN)
 
 lastOnTime = ""
 lastOffTime = ""
-currentStatus = "ON"
+currentStatus = "OFF"
 
 def checkConncetion():
     global currentStatus
     global bot
     # response = ping("91.227.183.197", 5, 0.2, privileged=True)
     # response = ping("195.189.227.49", 5, 0.2, privileged=True)
-    response = ping("10.8.3", 5, 0.2, privileged=True)
-    # print(f"Respone: {response}")
+    # response = ping("10.8.0.3", 5, 0.2, privileged=True)
+    print(f"Time: {datetime.datetime.now().strftime('%d.%m.%Y %H:%M')}")
     if response.is_alive:
         if currentStatus == "OFF":
             lastOnTime = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
@@ -24,7 +24,6 @@ def checkConncetion():
             print(f"+ {lastOnTime}")
             currentStatus = "ON"
         sleep(5)
-        checkConncetion()
     else:
         if currentStatus == "ON":
             lastOffTime = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
@@ -32,9 +31,9 @@ def checkConncetion():
             print(f"- {lastOffTime}")
             currentStatus = "OFF"
         sleep(5)
-        checkConncetion()
     return response.is_alive
 
 if __name__ == '__main__':
-    checkConncetion()
+    while True:
+        checkConncetion()
 
